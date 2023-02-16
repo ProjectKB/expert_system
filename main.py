2044a4f71c26282df25c4ebe7321802de35d4be2
Loic Bertin <loiberti@z4r8p1.42lyon.fr>
2023-02-16 19:48:50 +0100

    add colors to graph

--- main.py
+++ main.py
@@ -19,5 +19,3 @@ if __name__ == '__main__':
 
     system.backward_chaining()
     system.get_solution()
-
-
--- src/graph.py
+++ src/graph.py
@@ -8,13 +8,24 @@ class Graph:
     query: str
     rules: list[Rule]
 
+    ENDC = '\033[0m'
+    WHITE = '\033[97m'
+    COLORS = [
+        '\033[95m',
+        '\033[94m',
+        '\033[96m',
+        '\033[92m',
+        '\033[93m',
+        '\033[91m'
+    ]
+
     def show_graph(self):
-        print(f"\t[{self.query}]")
+        print(f"\t{self.WHITE}[{self.query}]{self.ENDC}")
         self.__travel_graph(self.rules, 0)
 
     def __travel_graph(self, rules: list[Rule], depth):
         for rule in rules:
-            print(f"\t{'-' * (depth + 1)}{rule}")
+            print(f"\t{self.COLORS[depth % 6]}{'-' * (depth + 1)}{rule}{self.ENDC}")
             if rule.children:
                 self.__travel_graph(rule.children, depth + 1)
 
